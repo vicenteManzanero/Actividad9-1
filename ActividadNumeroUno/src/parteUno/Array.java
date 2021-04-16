902acb55314277b195868f2f763596319b11535b
ciclost <ciclost@pc31>
2021-04-16 19:53:04 +0200

    Parte uno terminada

--- ActividadNumeroUno/src/parteUno/Array.java
+++ ActividadNumeroUno/src/parteUno/Array.java
@@ -15,9 +15,20 @@ public class Array {
             array[i]= aleatorio.nextInt(501);
             
         }
-        posicionAleatoria = aleatorio.nextInt(201)-50;
+        for (int i = 0; i < 10; i++) {
+            posicionAleatoria = aleatorio.nextInt(201)-50;
+            try{
+            
+             System.out.println("El valor de la array en la posición " + posicionAleatoria + " es " +array[posicionAleatoria] );
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("La posicion "+ posicionAleatoria + " está fuera de rango");
+            }
+            
+        }
         
-        System.out.println("El valor de la array en la posición " + posicionAleatoria + " es " +array[posicionAleatoria] );
+        
+       
         
     }
     
